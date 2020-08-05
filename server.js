ac4148aa41880462d70d6de6a305ba4dc95a8080
Islam Gönder <imiseay@gmail.com>
2020-08-05 11:28:10 +0200

    Added methods for bootcamps

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-//const { request } = require('express');
+const morgan = require('morgan');
+// const { request } = require('express');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
-
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } });
-});
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
